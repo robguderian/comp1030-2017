01207814c9552ed72611afb3bc191deec9c98a1f
Robert Guderian <robert.guderian@cantire.com>
2017-09-20 14:11:55 -0500

    Sept 19 notes

---
+++ 04_variables_math/notes.py
@@ -0,0 +1,24 @@
+CONST = "string"
+
+# This is comment out becusae........
+# CONST = "not a s tring
+
+
+# integer arithmetic
+
+a = 5/2
+print "first example " + str( type(a) )
+
+# how do we get around this
+
+b = float( 5/2 )
+
+print  b
+
+#nope
+
+c = 5/float(2)
+
+print c
+
+# yay
---
+++ 04_variables_math/notes2.py
@@ -0,0 +1,20 @@
+import math
+
+#print 5%2
+
+#print 234567865 % 10
+
+
+print math.sqrt(8)
+
+
+first = "hello "
+second = "world"
+
+print first + second
+
+# broken
+#print first + 1 
+
+# fixed!
+print "fixed " + str(1)
