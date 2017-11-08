9a96633a853a3b7199304fac304c052c0fe690aa
Rob Guderian <rob.guderian@gmail.com>
2017-11-08 13:08:41 -0600

    Adding try, and a text file

---
+++ 15_strings3/try.py
@@ -0,0 +1,6 @@
+try:
+    num = int(raw_input("Number > "))
+except ValueError:
+    print "there was a number problem!"
+
+
---
+++ 15_strings3/try2.py
@@ -0,0 +1,8 @@
+try:
+    num = int(raw_input("Number > "))
+except ValueError as problem:
+    print "there was a number problem!"
+    print problem
+   
+
+
